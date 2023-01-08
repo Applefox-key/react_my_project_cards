d2ac8ee3a6372aea35f1cab4990f27fb77ce01ca
Applefox-key <applefox.key@gmail.com>
2023-01-08 23:04:56 +0100

    blue spinner

--- src/components/UI/MySpinner.jsx
+++ src/components/UI/MySpinner.jsx
@@ -3,7 +3,7 @@ import Spinner from "react-bootstrap/Spinner";
 
 const MySpinner = () => {
   return (
-    <Spinner animation="border" role="status">
+    <Spinner animation="grow" variant="primary" role="status">
       <span className="visually-hidden">Loading...</span>
     </Spinner>
   );
