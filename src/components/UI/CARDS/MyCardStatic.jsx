b1a59d6c12de21d27f513f4350e69285ff8c18fd
Applefox-key <applefox.key@gmail.com>
2023-04-13 10:54:00 +0200

    font size optimisation has been added to the static card

--- src/components/UI/CARDS/MyCardStatic.jsx
+++ src/components/UI/CARDS/MyCardStatic.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import cl from "./MyCard.module.scss";
 import "./MyCard.module.scss";
-import { getImgQ } from "../../../utils/contentRequests";
+import { mainAndImg } from "../../../utils/cardFragment";
 
 const MyCardStatic = ({ item }) => {
   return (
     <div className={cl.container_gallery}>
       <div className={cl["card-container"]}>
-        <button
-          className={cl["card-button"]}
-          // onClick={() => setFlipped(!flipped)}
-        >
+        <button className={cl["card-button"]}>
           <div className={cl["card-front"]}>
-            {item.imgQ && <img src={getImgQ(item)} alt="" />}
+            {mainAndImg("front", 0, item, cl)}
             <h1 className="display-1">{item.question}</h1>
           </div>
         </button>
