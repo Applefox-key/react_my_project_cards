af985bb61958b81922addf2f346cb708bdc06b72
Applefox-key <applefox.key@gmail.com>
2023-05-28 19:40:13 +0200

    media query for the menu find has been added

--- src/components/SideBar/SideBarCollections/CategoryMiniMenu.jsx
+++ src/components/SideBar/SideBarCollections/CategoryMiniMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { CiMenuKebab } from "react-icons/ci";
-import { RiDeleteRow } from "react-icons/ri";
+import { RiDeleteBin2Line, RiDeleteRow } from "react-icons/ri";
 import cl from "../SideBar.module.scss";
 const CategoryMiniMenu = ({ el, setIsMenu, isMenu, deleteFn }) => {
   return (
@@ -15,12 +15,13 @@ const CategoryMiniMenu = ({ el, setIsMenu, isMenu, deleteFn }) => {
       {isMenu && (
         <div className={cl.miniMenu} id="miniMenu">
           <button
-            title="delete label"
+            title="delete label (collections will not be deleted)"
             onClick={(e) => {
               e.stopPropagation();
               deleteFn(el);
             }}>
-            <RiDeleteRow />
+            {/* <RiDeleteRow /> */}
+            <RiDeleteBin2Line />{" "}
             {el.id !== "all" ? "delete label" : "delete all"}
           </button>
         </div>
