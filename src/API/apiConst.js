d2c4d579dce732f7deea32192b96d1aa2a65264d
Applefox-key <applefox.key@gmail.com>
2024-09-26 14:20:28 -0500

    server url has been changed

--- src/API/apiConst.js
+++ src/API/apiConst.js
@@ -1,3 +1,4 @@
 // export const SERVER_URL = "http://localhost:8000";
-export const SERVER_URL = "https://34.214.160.243:8000";
+// export const SERVER_URL = "https://34.214.160.243:8000";
+export const SERVER_URL = "https://api.learnapp.me";
 export const imgSrv = SERVER_URL + "/img/";
