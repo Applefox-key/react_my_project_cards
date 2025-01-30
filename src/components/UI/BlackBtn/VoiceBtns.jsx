693b182c122caf87d86debb9a1fd93ce60d7a59c
Applefox-key <applefox.key@gmail.com>
2025-01-30 15:49:39 -0600

    voice button style has been changed

--- src/components/UI/BlackBtn/VoiceBtns.jsx
+++ src/components/UI/BlackBtn/VoiceBtns.jsx
@@ -1,42 +1,46 @@
 import React, { useMemo, useRef, useState } from "react";
-import { FaMicrophone } from "react-icons/fa";
+import { HiOutlineSpeakerWave } from "react-icons/hi2";
 import cl from "./BackBtn.module.scss";
 const VoiceBtns = ({ text }) => {
-  const langArr = useMemo(() => ["en", "ru", "pl", "ua"], []);
-  const [lang, setLang] = useState("en");
+  const [lang, setLang] = useState("en-US");
+  const langArr = useMemo(() => {
+    const synth = window.speechSynthesis;
+    const arrV = ["en-US", "ru-RU"];
+    const voices = synth.getVoices();
+    voices.forEach((voice) => {
+      if (!arrV.includes(voice.lang)) arrV.push(voice.lang);
+    });
+    console.log(arrV);
+
+    return arrV;
+  }, []);
   const btnRef = useRef(null);
 
   const onHandleClick = (e) => {
     e.stopPropagation();
     const synth = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance(text);
-    // const voices = synth.getVoices();
-    // voices.forEach((voice) => {
-    //   console.log(`Available voice: ${voice.name}, Language: ${voice.lang}`);
-    // });
+
     utterance.lang = lang;
     synth.speak(utterance);
   };
   return (
     <div ref={btnRef} className={cl["voice-mainBox"]}>
-      <div className={cl["voice-wrap"]}>
-        <div className={cl["langs"]}>
-          {langArr.map((el, i) => (
-            <p
-              key={i}
-              onClick={(e) => {
-                e.stopPropagation();
-                setLang(el);
-              }}
-              className={cl["langEl" + (lang === el)]}>
-              {el}
-            </p>
-          ))}
-        </div>
-
-        <button onClick={onHandleClick} title="Voice" className={cl.btnVoice}>
-          <FaMicrophone />
-        </button>
+      <button onClick={onHandleClick} title="Voice" className={cl.btnVoice}>
+        <HiOutlineSpeakerWave />
+      </button>
+      <div className={cl["langs"]}>
+        {langArr.map((el, i) => (
+          <div
+            key={i}
+            onClick={(e) => {
+              e.stopPropagation();
+              setLang(el);
+            }}
+            className={cl["langEl" + (lang === el)]}>
+            {el}
+          </div>
+        ))}
       </div>
     </div>
   );
