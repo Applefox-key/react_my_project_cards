e9407c446486dd2366cf95f05792a832a8d48cb5
Applefox-key <applefox.key@gmail.com>
2025-06-04 19:34:00 -0500

    new domen

--- src/API/apiConst.js
+++ src/API/apiConst.js
@@ -1,5 +1,6 @@
 // export const SERVER_URL = "http://localhost:8000";
 // export const SERVER_URL = "https://34.214.160.243:8000";
 // export const SERVER_URL = "https://api.learnapp.me";
-export const SERVER_URL = "http://95.216.136.56:8000";
+// export const SERVER_URL = "http://95.216.136.56:8000";
+export const SERVER_URL = "https://api.learnapp.pro";
 export const imgSrv = SERVER_URL + "/img/";
